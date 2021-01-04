647d10206347166b718a15034a547da549c6f2dd
Felipe Guimarães <felipeguimaraes@Felipes-MacBook-Pro.local>
2021-01-04 14:49:53 -0300

    Refatoração de codigo para orientação a objetos e qualidade, correções de lógica, implementação de processamento paralelo e concorrencia, para maior performance e velocidade de execução

---
+++ lib_hunter.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+import socket, os
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+class Hunter:
+
+    def __init__(self):
+        self.base_dir = os.path.dirname(os.path.abspath(__file__))
+        self.wordlist = os.path.join(self.base_dir, 'dns_bruteforce.txt')
+        self.banner = os.path.join(self.base_dir, 'banner.txt')
+
+    def get_banner(self):
+        os.system('clear')
+        with open(self.banner, 'r') as banner:
+            return '\033[35m'+ banner.read()
+
+    def is_root(self):
+        permissao_do_usuario = os.geteuid()
+        if permissao_do_usuario == 0:
+            return True
+        return False
+
+    def get_user_parameters(self, explanation):
+        return input(explanation)
+
+    def heartbeat(self, target_domain, dominio_in_bruteforce):
+        for nome in dominio_in_bruteforce:
+            DNS = nome.strip("\n") + "." + target_domain
+            try:
+                print("#| " + DNS + ": " + socket.gethostbyname(DNS))
+                with open('dns_hunter_result.txt', 'w') as output:
+                    output.write("#| " + DNS + ": " + socket.gethostbyname(DNS))
+
+            except socket.gaierror:
+                pass
+        return ("\n\nEnumeração feita com sucesso!!")
+
+
+    def hunt(self):
+        hunter = Hunter()
+        print(self.get_banner())
+        if not self.is_root():
+            for i in range(5):
+                print("              Este programa precisa ser executado em modo ROOT!!")
+            os._exit(0)
+
+        target = self.get_target()
+        return self.heartbeat(target)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    hunt = Hunter()
+    print(hunt.hunt())
+
+
